8eb91891ab9bb47ef74c5f08e70bf7dc2fbed8dc
zalaxci <ima@cyber-wizard.com>
2020-07-09 22:22:42 +0300

    Working on autocorrect

--- search/search.js
+++ search/search.js
@@ -1,26 +1,15 @@
 //Get search bar
 var input = document.getElementById('search-box');
 var autocomplete = document.getElementById("autocomplete");
-//List of common searches
-var suggestions = ["youtube", "yt", "twitter"]
+//List of suggestions
+var suggestions = ["youtube", "yt", "twitter"];
 //Clear previous text
 input.value = "";
 //On key press
 function keyPress(e) {
   //If key is enter, search
   if (e.keyCode === 13) {
-    var url;
-    switch(input.value) {
-      case "twitter":
-        url = "https://twitter.com"
-        break;
-      case "youtube":
-        url = "https://youtube.com"
-        break;
-      default:
-        url = "https://www.ecosia.org/search?q=" + input.value;
-    }
-    window.location.href = url;
+    //goTo(input.value);
   }
   //Focus on search if it is not focused
   if (input !== document.activeElement) {
@@ -35,8 +24,10 @@ function keyDown() {
   autocomplete.innerHTML = '';
   if(input.value != ""){
     for (i = 0; i < suggestions.length; i++) {
-      if (suggestions[i].substr(0, input.value.length) == input.value){suggestion = document.createElement("div");
+      if (suggestions[i].substr(0, input.value.length) == input.value){
+        suggestion = document.createElement("div");
         suggestion.innerHTML = "<strong>" + input.value + "</strong>" + suggestions[i].substr(input.value.length);
+        suggestion.onclick = function() { alert('blah'); };
         autocomplete.appendChild(suggestion);
       }
     }
